c673f24979a668541ad2453beb4bfc1602c46471
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-03-07 14:27:46 +0100

    resolve

---
+++ web/bank_loan_pred/website/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from website import views
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', views.home, name='homepage'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('special/', views.special, name='special'),
+    path('contact/<int:test>', views.contact, name='contact'),
+    path('api_pred/', views.api_predict, name='api_predict'),
+    path('api_data/', views.api_data, name='api_data'),
+]
