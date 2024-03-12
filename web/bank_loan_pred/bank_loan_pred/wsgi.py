8d13f795b13332b12b48f1ad935fec76ebef6bd7
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-03-12 13:29:40 +0100

    modif wsgi, dockerfile-django, entrypoint.sh

--- web/bank_loan_pred/bank_loan_pred/wsgi.py
+++ web/bank_loan_pred/bank_loan_pred/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
+import pathlib
+#ceci a impact quand utilise gunicorn (pr azure)
+CURRENT_DIR = pathlib.Path(__file__).resolve().parent
+BASE_DIR = CURRENT_DIR.parent
+ENV_FILE_PATH = BASE_DIR / '.env'
+dotenv.read_dotenv(str(ENV_FILE_PATH), override=True)
 
 from django.core.wsgi import get_wsgi_application
 
--- web/bank_loan_pred/entrypoint.sh
+++ web/bank_loan_pred/entrypoint.sh
@@ -2,5 +2,5 @@
 
 # Effectuer les migrations
 echo "Effectuer les migrations de la base de données..."
-python manage.py makemigrations && python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python bank_loan_pred/manage.py makemigrations && python manage.py migrate
+python bank_loan_pred/manage.py runserver 0.0.0.0:8001
